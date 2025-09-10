81ff12427e2a923bfdaea9af1e0241adb58e5a66
Ian Harrigan <ianharrigan@hotmail.com>
2025-09-10 15:56:01 +0200

    stringbuf composition vs inheritance (for macros)

--- src/xml/XmlStringBuf.hx
+++ src/xml/XmlStringBuf.hx
@@ -1,7 +1,38 @@
 package xml;
 
-class XmlStringBuf extends StringBuf {
+class XmlStringBuf {
+    private var buffer:StringBuf;
+
+    public function new():Void {
+        buffer = new StringBuf();
+    }
+
+	public var length(get, never):Int;
+    private function get_length() {
+        return buffer.length;
+    }
+
+	public function add<T>(x:T):Void {
+        buffer.add(x);
+    }
+
+	public function addChar(c:Int):Void {
+        buffer.addChar(c);
+    }
+
+	public function addSub(s:String, pos:Int, ?len:Int):Void {
+        buffer.addSub(s, pos, len);
+    }
+
+	public function toString():String {
+        return buffer.toString();
+    }
+
     public function reset() {
-        offset = 0;
+        #if js
+        @:privateAccess buffer.offset = 0;
+        #else
+        buffer = new StringBuf();
+        #end
     }
 }
