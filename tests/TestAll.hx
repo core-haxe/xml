f9cf2b63cf6df0f53f2757ddd00e9f0ba0895e3f
Ian Harrigan <ianharrigan@hotmail.com>
2025-09-10 15:56:14 +0200

    reenable all tests

--- tests/TestAll.hx
+++ tests/TestAll.hx
@@ -9,14 +9,10 @@ class TestAll {
     public static function main() {
         var runner = new Runner();
         
-        /*
         runner.addCase(new TestBasic());
         runner.addCase(new TestXPath());
-        */
         runner.addCase(new TestPositionInfo());
-        /*
         runner.addCase(new TestMalformed());
-        */
 
         Report.create(runner, SuccessResultsDisplayMode.AlwaysShowSuccessResults, HeaderDisplayMode.NeverShowHeader);
         runner.run();
